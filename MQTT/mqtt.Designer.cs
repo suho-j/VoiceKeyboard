52f69eef3d9822d582bec0c60739ff7c3585705d
DESKTOP-TDO0MH8\ho_office <ho910728@gmail.com>
2018-11-14 11:21:20 +0900

    first commit

---
+++ MQTT/mqtt.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MQTT
+{
+    partial class mqtt
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label7;
+            System.Windows.Forms.Label label6;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label1;
+            this.tbRecvMessage = new System.Windows.Forms.TextBox();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.btnConnectControl = new System.Windows.Forms.Button();
+            this.gbSettings = new System.Windows.Forms.GroupBox();
+            this.tbPw = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            label7 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            this.gbSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(12, 6);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(65, 12);
+            label7.TabIndex = 15;
+            label7.Text = "Messages";
+            // 
+            // label6
+            // 
+            label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(570, 253);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(40, 12);
+            label6.TabIndex = 13;
+            label6.Text = "Status";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(15, 110);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(62, 12);
+            label4.TabIndex = 7;
+            label4.Text = "PassWord";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(15, 83);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(65, 12);
+            label3.TabIndex = 4;
+            label3.Text = "UserName";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(15, 29);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(16, 12);
+            label2.TabIndex = 3;
+            label2.Text = "IP";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(15, 56);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(27, 12);
+            label1.TabIndex = 1;
+            label1.Text = "Port";
+            // 
+            // tbRecvMessage
+            // 
+            this.tbRecvMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRecvMessage.Font = new System.Drawing.Font("굴림", 13F);
+            this.tbRecvMessage.Location = new System.Drawing.Point(12, 27);
+            this.tbRecvMessage.Multiline = true;
+            this.tbRecvMessage.Name = "tbRecvMessage";
+            this.tbRecvMessage.Size = new System.Drawing.Size(550, 422);
+            this.tbRecvMessage.TabIndex = 14;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbStatus.Enabled = false;
+            this.tbStatus.Location = new System.Drawing.Point(572, 268);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.Size = new System.Drawing.Size(232, 181);
+            this.tbStatus.TabIndex = 12;
+            // 
+            // btnConnectControl
+            // 
+            this.btnConnectControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnectControl.Location = new System.Drawing.Point(572, 196);
+            this.btnConnectControl.Name = "btnConnectControl";
+            this.btnConnectControl.Size = new System.Drawing.Size(232, 42);
+            this.btnConnectControl.TabIndex = 1;
+            this.btnConnectControl.Text = "연결";
+            this.btnConnectControl.UseVisualStyleBackColor = true;
+            this.btnConnectControl.Click += new System.EventHandler(this.btnConnectControl_Click);
+            // 
+            // gbSettings
+            // 
+            this.gbSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbSettings.Controls.Add(label4);
+            this.gbSettings.Controls.Add(this.tbPw);
+            this.gbSettings.Controls.Add(this.tbId);
+            this.gbSettings.Controls.Add(label3);
+            this.gbSettings.Controls.Add(this.tbPort);
+            this.gbSettings.Controls.Add(this.tbIp);
+            this.gbSettings.Controls.Add(label2);
+            this.gbSettings.Controls.Add(label1);
+            this.gbSettings.Location = new System.Drawing.Point(572, 55);
+            this.gbSettings.Name = "gbSettings";
+            this.gbSettings.Size = new System.Drawing.Size(230, 135);
+            this.gbSettings.TabIndex = 16;
+            this.gbSettings.TabStop = false;
+            this.gbSettings.Text = "Settings";
+            // 
+            // tbPw
+            // 
+            this.tbPw.Location = new System.Drawing.Point(96, 107);
+            this.tbPw.Name = "tbPw";
+            this.tbPw.PasswordChar = '*';
+            this.tbPw.Size = new System.Drawing.Size(128, 21);
+            this.tbPw.TabIndex = 6;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(96, 80);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(128, 21);
+            this.tbId.TabIndex = 5;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(96, 53);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(128, 21);
+            this.tbPort.TabIndex = 2;
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(96, 26);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(128, 21);
+            this.tbIp.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::MQTT.Properties.Resources.보이스키보드_로고;
+            this.pictureBox1.Location = new System.Drawing.Point(662, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mqtt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gbSettings);
+            this.Controls.Add(this.btnConnectControl);
+            this.Controls.Add(label7);
+            this.Controls.Add(this.tbRecvMessage);
+            this.Controls.Add(label6);
+            this.Controls.Add(this.tbStatus);
+            this.MinimumSize = new System.Drawing.Size(500, 380);
+            this.Name = "mqtt";
+            this.ShowIcon = false;
+            this.Text = "PNUH 보이스 키보드";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mqtt_FormClosing);
+            this.Load += new System.EventHandler(this.mqtt_Load);
+            this.gbSettings.ResumeLayout(false);
+            this.gbSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRecvMessage;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.Button btnConnectControl;
+        private System.Windows.Forms.GroupBox gbSettings;
+        private System.Windows.Forms.TextBox tbPw;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MQTT/mqtt.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace MQTT
+{
+    public partial class mqtt : Form
+    {
+        #region Field
+        MqttClient client;
+        string ip = null ;
+        int port = 0;
+        string clientId;
+        int selStart = 0;
+        bool isconnected = false;
+        string username; string password;
+        string[] topics = new string[2];
+        byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+        #endregion
+
+        public mqtt()
+        {
+            InitializeComponent();
+        }
+
+        private void mqtt_Load(object sender, EventArgs e)
+        {
+            txtReader();
+        }
+
+        
+        private StringBuilder _Strings;
+        private String Strings
+        {
+            set
+            {
+                if (_Strings == null)
+                    _Strings = new StringBuilder(1024);
+                // 로그 길이가 1024자가 되면 이전 로그 삭제
+                if (_Strings.Length >= (1024 - value.Length))
+                    _Strings.Clear();
+                // 로그 추가 및 화면 표시
+                _Strings.AppendLine(value);
+                tbStatus.Text = _Strings.ToString();
+            }
+        }
+
+        private Boolean IsOpen
+        {
+            set
+            {
+                if (value)
+                {
+                    Strings = "연결 됨";
+                    btnConnectControl.Text = "연결 끊기";
+                    //tbRecvMessage.Enabled = true;
+                    gbSettings.Enabled = false;
+                }
+                else
+                {
+                    Strings = "연결 해제됨";
+                    btnConnectControl.Text = "연결";
+                    //tbRecvMessage.Enabled = false;
+                    gbSettings.Enabled = true;
+                }
+            }
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            this.Invoke(new EventHandler(delegate
+            {
+                string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+
+                Invalidate();
+                selStart = tbRecvMessage.SelectionStart;
+                tbRecvMessage.Text = tbRecvMessage.Text.Insert(selStart, ReceivedMessage);
+
+                selStart = selStart + ReceivedMessage.Length;   // 시작위치 + 붙여넣은 string의 길이 반환
+                tbRecvMessage.Select(selStart, 0); // 커서값 새로 설정
+            }));
+        }
+
+        private void btnConnectControl_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbIp.Text.Trim()))
+            {
+                MessageBox.Show("input the IP");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbPort.Text.Trim()))
+            {
+                MessageBox.Show("input the Port");
+                return;
+            }
+
+            try
+            {
+                if (!isconnected)
+                {
+                    ip = tbIp.Text;
+                    port = int.Parse(tbPort.Text);
+                    username = tbId.Text;
+                    password = tbPw.Text;
+
+                    client = new MqttClient(IPAddress.Parse(ip), port, false, null, null, 0);
+
+                    client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+                    clientId = Guid.NewGuid().ToString();
+
+                    var returnNum = client.Connect(clientId, username, password);
+
+                    if (returnNum != 0)
+                    {
+                        Strings = "Please check UserName or PassWord ...";
+                        gbSettings.Enabled = true;
+                        btnConnectControl.Enabled = true;
+                        return;
+                    }
+
+                    topics[0] = username + "/pnuh_stt";
+                    topics[1] = username + "/pnuh_stt";
+                    client.Subscribe(topics, qosLevels);
+
+                    isconnected = !isconnected;
+                    IsOpen = isconnected;
+                    txtWriter();
+                }
+                else
+                {
+                    client.Unsubscribe(topics);
+                    client.Disconnect();
+
+                    isconnected = !isconnected;
+                    IsOpen = isconnected;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "Exception connecting to the broker")
+                {
+                    Strings = "Please check IP or Port or Server ...";
+                }
+                btnConnectControl.Enabled = true;
+                gbSettings.Enabled = true;
+            }
+        }
+        
+        /// <summary>
+        /// ip, port, id 불러오기
+        /// </summary>
+        public void txtReader()
+        {
+            string line;
+            string path = Application.StartupPath + @"\Data\settings.txt";
+            try
+            {
+                using (StreamReader rdr = new StreamReader(path))
+                {
+                    // ReadLine()을 써서 한 라인을 읽어 들인다
+                    // 만약 파일 끝이면 null이 리턴된다
+                    int i = 0;
+                    while ((line = rdr.ReadLine()) != null)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                this.Invoke(new Action(() => this.tbIp.Text = line));
+                                break;
+                            case 1:
+                                this.Invoke(new Action(() => this.tbPort.Text = line));
+                                break;
+                            case 2:
+                                this.Invoke(new Action(() => this.tbId.Text = line));
+                                break;
+                            default:
+                                break;
+                        }
+                        i++;
+                    }
+                }
+            }
+            catch (IOException) { }
+        }
+
+        /// <summary>
+        /// ip, port, id 저장
+        /// </summary>
+        public void txtWriter()
+        {
+            string path = Application.StartupPath + @"\Data\settings.txt";
+            try
+            {
+                // 텍스트 파일 쓰기            
+                // StreamWriter 객체를 생성한다. 입력파라미터로 File Path나 파일스트림 사용한다.
+                using (StreamWriter wr = new StreamWriter(path))
+                {
+                    // WriteLine()을 써서 한 라인씩 문자열을 쓴다.                
+                    wr.WriteLine(tbIp.Text);
+                    wr.WriteLine(tbPort.Text);
+                    wr.WriteLine(tbId.Text);
+                }
+            }
+            catch (IOException) { }
+        }
+
+        private void mqtt_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isconnected)
+            {
+                client.Unsubscribe(topics);
+                client.Disconnect();
+            }
+        }
+
+    }
+}
