7167522e6ab25017950756399f8afe0f66ff4af7
DESKTOP-TDO0MH8\ho_office <ho910728@gmail.com>
2018-11-14 12:12:34 +0900

    우디팝과 연동 UI 수정사항 반영 기능 구현은 기획방향 정해지고, 기획서 전달 받은 후에 진행

--- MQTT/mqtt.Designer.cs
+++ MQTT/mqtt.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label label7;
-            System.Windows.Forms.Label label6;
             System.Windows.Forms.Label label4;
             System.Windows.Forms.Label label3;
             System.Windows.Forms.Label label2;
             System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label5;
+            System.Windows.Forms.Label label6;
+            System.Windows.Forms.Label label8;
+            System.Windows.Forms.Label label10;
+            System.Windows.Forms.Label label11;
+            this.label9 = new System.Windows.Forms.Label();
             this.tbRecvMessage = new System.Windows.Forms.TextBox();
             this.tbStatus = new System.Windows.Forms.TextBox();
             this.btnConnectControl = new System.Windows.Forms.Button();
@@ -43,12 +48,20 @@
             this.tbPort = new System.Windows.Forms.TextBox();
             this.tbIp = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
             label7 = new System.Windows.Forms.Label();
-            label6 = new System.Windows.Forms.Label();
             label4 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label1 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            label10 = new System.Windows.Forms.Label();
+            label11 = new System.Windows.Forms.Label();
             this.gbSettings.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -61,16 +74,6 @@
             label7.Size = new System.Drawing.Size(65, 12);
             label7.TabIndex = 15;
             label7.Text = "Messages";
-            // 
-            // label6
-            // 
-            label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            label6.AutoSize = true;
-            label6.Location = new System.Drawing.Point(570, 253);
-            label6.Name = "label6";
-            label6.Size = new System.Drawing.Size(40, 12);
-            label6.TabIndex = 13;
-            label6.Text = "Status";
             // 
             // label4
             // 
@@ -108,6 +111,64 @@
             label1.TabIndex = 1;
             label1.Text = "Port";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(12, 306);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(41, 12);
+            label5.TabIndex = 19;
+            label5.Text = "Memo";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            label6.Location = new System.Drawing.Point(79, 9);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(485, 12);
+            label6.TabIndex = 20;
+            label6.Text = "────────────────────────────────────────";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            label8.Location = new System.Drawing.Point(55, 306);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(509, 12);
+            label8.TabIndex = 21;
+            label8.Text = "──────────────────────────────────────────";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            label10.Location = new System.Drawing.Point(607, 306);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(197, 12);
+            label10.TabIndex = 24;
+            label10.Text = "────────────────";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new System.Drawing.Point(570, 306);
+            label11.Name = "label11";
+            label11.Size = new System.Drawing.Size(33, 12);
+            label11.TabIndex = 23;
+            label11.Text = "Save";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label9.Location = new System.Drawing.Point(63, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(161, 12);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "─────────────";
+            // 
             // tbRecvMessage
             // 
             this.tbRecvMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -117,7 +178,7 @@
             this.tbRecvMessage.Location = new System.Drawing.Point(12, 27);
             this.tbRecvMessage.Multiline = true;
             this.tbRecvMessage.Name = "tbRecvMessage";
-            this.tbRecvMessage.Size = new System.Drawing.Size(550, 422);
+            this.tbRecvMessage.Size = new System.Drawing.Size(550, 250);
             this.tbRecvMessage.TabIndex = 14;
             // 
             // tbStatus
@@ -125,11 +186,11 @@
             this.tbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbStatus.Enabled = false;
-            this.tbStatus.Location = new System.Drawing.Point(572, 268);
+            this.tbStatus.Location = new System.Drawing.Point(572, 237);
             this.tbStatus.Multiline = true;
             this.tbStatus.Name = "tbStatus";
             this.tbStatus.ReadOnly = true;
-            this.tbStatus.Size = new System.Drawing.Size(232, 181);
+            this.tbStatus.Size = new System.Drawing.Size(232, 40);
             this.tbStatus.TabIndex = 12;
             // 
             // btnConnectControl
@@ -137,7 +198,7 @@
             this.btnConnectControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnConnectControl.Location = new System.Drawing.Point(572, 196);
             this.btnConnectControl.Name = "btnConnectControl";
-            this.btnConnectControl.Size = new System.Drawing.Size(232, 42);
+            this.btnConnectControl.Size = new System.Drawing.Size(232, 35);
             this.btnConnectControl.TabIndex = 1;
             this.btnConnectControl.Text = "연결";
             this.btnConnectControl.UseVisualStyleBackColor = true;
@@ -146,6 +207,7 @@
             // gbSettings
             // 
             this.gbSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbSettings.Controls.Add(this.label9);
             this.gbSettings.Controls.Add(label4);
             this.gbSettings.Controls.Add(this.tbPw);
             this.gbSettings.Controls.Add(this.tbId);
@@ -194,29 +256,79 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::MQTT.Properties.Resources.보이스키보드_로고;
-            this.pictureBox1.Location = new System.Drawing.Point(662, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(609, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(134, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(142, 37);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMemo.Font = new System.Drawing.Font("굴림", 13F);
+            this.tbMemo.Location = new System.Drawing.Point(12, 332);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.Size = new System.Drawing.Size(550, 117);
+            this.tbMemo.TabIndex = 18;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(572, 332);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(232, 35);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "저장(*.txt)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(572, 373);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(232, 35);
+            this.btnCopy.TabIndex = 26;
+            this.btnCopy.Text = "전체 복사";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(572, 414);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(232, 35);
+            this.btnRemove.TabIndex = 27;
+            this.btnRemove.Text = "전체 지우기";
+            this.btnRemove.UseVisualStyleBackColor = true;
             // 
             // mqtt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 461);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(label10);
+            this.Controls.Add(label11);
+            this.Controls.Add(label8);
+            this.Controls.Add(label6);
+            this.Controls.Add(label5);
+            this.Controls.Add(this.tbMemo);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.gbSettings);
             this.Controls.Add(this.btnConnectControl);
             this.Controls.Add(label7);
             this.Controls.Add(this.tbRecvMessage);
-            this.Controls.Add(label6);
             this.Controls.Add(this.tbStatus);
             this.MinimumSize = new System.Drawing.Size(500, 380);
             this.Name = "mqtt";
             this.ShowIcon = false;
-            this.Text = "PNUH 보이스 키보드";
+            this.Text = "Voice Keyboard";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mqtt_FormClosing);
             this.Load += new System.EventHandler(this.mqtt_Load);
             this.gbSettings.ResumeLayout(false);
@@ -238,6 +350,11 @@
         private System.Windows.Forms.TextBox tbPort;
         private System.Windows.Forms.TextBox tbIp;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbMemo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnRemove;
     }
 }
 
